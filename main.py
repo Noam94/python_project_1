dc43fd0997f8e4123959ddbfc1bd3dc042716cbd
Noam94 <noam.g79@gmail.com>
2024-06-05 18:05:48 +0300

    initiate project

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+
+my_api = '47d8441287d3c3d5fe39e3668e7e78c3'
+city = input("Please write a city name: ")
+weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={my_api}").json()
+
+while weather_data['cod'] != 200:
+  print(weather_data['message'])
+  city = input("Please write a city name: ")
+  weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={my_api}").json()
+
+
+weather_string = f'''
+The weather in {city} is: {weather_data['weather'][0]['description']}
+Temprature: {weather_data['main']['temp']} degrees
+Humidity: {weather_data['main']['humidity']} %
+'''
+
+print(weather_string)
