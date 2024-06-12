973b9aec027746c6ea983cdf7d2737eac55567f5
Noam94 <noam.g79@gmail.com>
2024-06-12 22:06:26 +0300

    after corrections
    making the code a bit shorter

--- main.py
+++ main.py
@@ -6,60 +6,53 @@ import json
 st.title('Welcome to the weather app!')
 my_api = '47d8441287d3c3d5fe39e3668e7e78c3'
 
+# display current date and time
 st.write(dt.datetime.now(dt.timezone(dt.timedelta(hours=+3))).strftime('''
 %A, %d %B %Y\n
 %I:%M %p
 '''))
 
-location = {}
+def save_settings(settings, file='settings.json'):
+  with open(file, 'w') as f:
+    json.dump(settings, f)
+
+settings = {}
 city = ''
 try:
   with open('settings.json', 'r+') as f:
     try:
-      settings = (json.load(f))
-      location["default"] = settings.setdefault("default", [])
-      location["favorites"] = settings.setdefault("favorites", [])
-      location["units"] = settings.setdefault("units", [])
+      settings_file = (json.load(f))
+      settings["default"] = settings_file.setdefault("default", [])
+      settings["favorites"] = settings_file.setdefault("favorites", [])
+      settings["units"] = settings_file.setdefault("units", [])
     except:
-      location["default"] = []
-      location["favorites"] = []
-      location["units"] = []
+      settings = {"default": "", "favorites": [], "units": ""}
 except FileNotFoundError:
   with open('settings.json', 'w+') as f:
-    location["default"] = []
-    location["favorites"] = []
-    location["units"] = []
+    settings = {"default": "", "favorites": [], "units": ""}
 
 # set default locations:
-default_loc_input = st.text_input("Would you like to set a default location?")
-if default_loc_input != '' and st.button("Set default"):
-  location["default"] = default_loc_input.title()
-  with open('settings.json','w') as f:
-    json.dump(location,f)
+default_loc_input = st.text_input("Set a default location:")
+if default_loc_input and st.button("Set default"):
+  settings["default"] = default_loc_input.title()
+  save_settings(settings)
 
 # set favorites:
 favorites_loc_input = st.text_input("Add favorite locations:")
-if favorites_loc_input != '' and st.button("Add"):
-  if favorites_loc_input.title() not in location["favorites"]:
-    location["favorites"].append(favorites_loc_input.title())
-    with open('settings.json','w') as f:
-      json.dump(location,f)
+if favorites_loc_input and st.button("Add"):
+  if favorites_loc_input.title() not in settings["favorites"]:
+    settings["favorites"].append(favorites_loc_input.title())
+    save_settings(settings)
 
 # set default units:
-units_input = st.radio("Temperature units", ["metric", "imperial"], captions=["°C","°F"], index=None)
-if units_input is not None:
-  def_units = units_input
-elif len(location["units"]) > 0:
-  def_units = location["units"]
-else:
-  def_units = "metric"
+units_input = st.radio("Temperature units", ["metric", "imperial"], captions=["°C","°F"], index=1 if settings["units"] == "imperial" else 0)
 
 if st.button("Set default units"):
-  location["units"] = def_units
-  with open('settings.json', 'w') as f:
-    json.dump(location, f)
+  settings["units"] = units_input
+  save_settings(settings)
 
-def get_weather(weather_data, units, city=location["default"]):
+
+def get_weather(weather_data, units, city=settings["default"]):
   st.header(city.title())
   st.subheader('Weather description')
   if units == "imperial":
@@ -81,29 +74,28 @@ def get_timezone(offset):
         %I:%M %p
         '''))
 
-if len(location["favorites"]) > 0:
+
+if settings["favorites"]:
   check_favorites = st.checkbox("Choose from favorite")
   if check_favorites:
-    city = st.selectbox("Choose from favorites", location["favorites"], index=None, placeholder="Select location...")
+    city = st.selectbox("Choose from favorites", settings["favorites"], index=None, placeholder="Select location...")
   else:
-    city = st.text_input("Where would you like to know the weather in?")
+    city = st.text_input("Enter city name:")
 else:
-  city = st.text_input("Where would you like to know the weather in?")
-  check_favorites = False
+  city = st.text_input("Enter city name:")
 
-weather_data = rs.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&units={def_units}&appid={my_api}").json()
-
+weather_data = rs.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&units={units_input}&appid={my_api}").json()
 
 show_time = st.toggle("Local time")
 
-if (city != '' and not check_favorites) or (city is not None and check_favorites):
+if (city):
   if weather_data['cod'] == 200:
-    get_weather(weather_data, def_units, city)
+    get_weather(weather_data, units_input, city)
     if show_time:
       get_timezone(weather_data['timezone'] / (60 * 60))
   else:
     st.write(weather_data['message'])
-elif len(location["default"]) >= 1:
-  default_loc = location["default"]
-  weather_data = rs.get(f"https://api.openweathermap.org/data/2.5/weather?q={default_loc}&units={def_units}&appid={my_api}").json()
-  get_weather(weather_data, def_units)
+elif settings["default"]:
+  default_loc = settings["default"]
+  weather_data = rs.get(f"https://api.openweathermap.org/data/2.5/weather?q={default_loc}&units={units_input}&appid={my_api}").json()
+  get_weather(weather_data, units_input)
